e9e60a48daf0c28c1b3caeaff817a7a3ce6b20b8
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-14 02:41:33 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,8 +3,12 @@
 colours = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 def display_rainbow(colours)
-  puts "R: #{colours[0]}, O: #{colours[1]}, Y: #{colours[2]}"
-  puts "G: #{colours[3]}, B: #{colours[4]}, I: #{colours[5]}"
+  puts "R: #{colours[0]}"
+  puts "O: #{colours[1]}"
+  puts "Y: #{colours[2]}"
+  puts "G: #{colours[3]}"
+  puts "B: #{colours[4]}"
+  puts "I: #{colours[5]}"
   puts "V: #{colours[6]}"
 end
 
