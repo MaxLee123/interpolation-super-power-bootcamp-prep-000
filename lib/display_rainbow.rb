936543dc5a794cf4f28d6231164566721408e54f
MInh PHO DUC LE <minh.pdl@gmail.com>
2019-05-14 03:13:54 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,7 +3,7 @@
 colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
 def display_rainbow(col)
-  puts "R: #{colors[0]}, O: #{colors[1], Y: #{colors[2]}""
+  puts "R: #{col[0]}, O: #{colors[1], Y: #{colors[2]}""
 end
 
 display_rainbow(colors)
